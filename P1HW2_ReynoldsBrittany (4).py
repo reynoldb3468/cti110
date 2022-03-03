e21d8c510ed9597f44c87c6e465b9b1c5b359286
reynoldb3468 <99416784+reynoldb3468@users.noreply.github.com>
2022-03-03 12:40:03 -0500

    Add files via upload

---
+++ P1HW2_ReynoldsBrittany (4).py
@@ -0,0 +1,12 @@
+
+# Pizza Calculations
+# 2/10/22
+# CTI-110 P1HW2 - Pizza Order
+# Brittany Reynolds
+#
+numStudents = int(input("How many students do you want to order pizza for? "))
+numSlices = numStudents * 3
+numPizzas = numStudents / 2
+
+print("----Pizza Order------")
+print("Number of Students: ",numStudents)
