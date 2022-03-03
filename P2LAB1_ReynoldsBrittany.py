e2fc1a0f7b08a547fc5942f42bfb72ee62d7bea2
reynoldb3468 <99416784+reynoldb3468@users.noreply.github.com>
2022-03-03 12:36:17 -0500

    Add files via upload

---
+++ P2LAB1_ReynoldsBrittany.py
@@ -0,0 +1,10 @@
+user_int=int(input("Enter integer (32 - 126):\n")) #input user_int 
+user_float=float(input("Enter float:\n")) #input user_float 
+user_character=input("Enter character:\n") #input user_character 
+user_string=input("Enter string:\n") #input user_string 
+#display values 
+print(user_int,"",user_float,"",user_character,"",user_string) 
+#reverse values 
+print(user_string,"",user_character,"",user_float,"",user_int) 
+#convert user_int to character 
+print(user_int,"converted to a character is",chr(user_int)) 
