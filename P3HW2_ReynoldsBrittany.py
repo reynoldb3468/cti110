6d029a563d0462bac9a9519f966661319a6fbfd1
reynoldb3468 <99416784+reynoldb3468@users.noreply.github.com>
2022-03-11 22:08:47 -0500

    Add files via upload

---
+++ P3HW2_ReynoldsBrittany.py
@@ -0,0 +1,43 @@
+# CTI-110
+# P3HW2 - Pizza Order
+# Brittany Reynolds 
+# 3/11/22
+#
+
+start
+
+print("How many students do you want to order pizza for?") 
+
+take input number_of_students 
+
+print("Enter number of people per pizza ( 1.5, 2 or 3):") 
+
+take input people_per_pizza 
+
+if people_per_pizza != 1.5 AND people_per_pizza != 2 AND people_per_pizza != 3 
+
+print("INVALID ENTRY!!!!") 
+
+print("Should have entered 1.5, 2 or 3")
+
+print("Run program again")
+
+else 
+
+number pizza_needed = round of number_of_students / people_per_pizza 
+
+number amount = 5 * pizza_needed 
+
+amount = amount + 0.06 * amount 
+
+print("----PIZZA ORDER--------") 
+
+print("Number of students : " + number_of_students)
+
+print("Pizzas Needed : " + pizza_needed)
+
+print("Price $" + amount)
+
+print("-------------------------------")
+
+end
