0c4f896e027f9e1e734ef0d368859684a8910f1d
reynoldb3468 <99416784+reynoldb3468@users.noreply.github.com>
2022-03-17 12:27:31 -0400

    Add files via upload

---
+++ p4LAB2a_Reynolds.py
@@ -0,0 +1,17 @@
+ import turtle             
+ wn = turtle.Screen()      
+ alex = turtle.Turtle()    
+
+for i in range (4):
+    alex.forward(50)
+    alex.left(90)
+
+
+for i in range (3):
+    alex.forward(300)
+    alex.left(120)
+
+wn.mainloop()
+ 
+ 
+ 
